75ec3b0fee0bf1f6db64ea012541e03f3e5eafce
rutumistry <rutumistry@gmail.com>
2025-01-03 03:03:39 +0000

    modified prompt

---
+++ day0/main.py
@@ -0,0 +1,5 @@
+def getLocation(place=None):
+    return {"city": "New York City", "state": "NY", "country": "USA"}
+
+def getCurrentWeather(place=None):
+    return {"temperature": "2", "unit": "F", "forecast": "snowy"}
